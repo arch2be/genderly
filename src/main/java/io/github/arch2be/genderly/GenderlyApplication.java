63d10f17aa7fa2ad594c9c764822fd4603dd8993
arch2be <jdvozef@gmail.com>
2021-02-18 21:43:07 +0100

    Initial commit

---
+++ src/main/java/io/github/arch2be/genderly/GenderlyApplication.java
@@ -0,0 +1,13 @@
+package io.github.arch2be.genderly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenderlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenderlyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/arch2be/genderly/GenderlyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.arch2be.genderly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenderlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
